f8b74c94acf3f6aa37b44fe449a9ea208a13cd5b
grid34 <fremitage@gmail.com>
2022-11-12 14:46:26 -0600

    Update test.java

--- src/tc1/test.java
+++ src/tc1/test.java
@@ -3,8 +3,8 @@ package tc1;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+            System.out.println("hello");
+            System.out.println("hi");
 	}
 
 }
